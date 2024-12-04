627c3fdd1da6744b21a202862d7bb17a51a065d2
Radu Constantin <raduadrian.constantin94@gmail.com>
2024-12-04 10:45:15 +0200

    Solved day 1

--- day_1.php
+++ day_1.php
@@ -9,3 +9,32 @@ while($line = fgets($text)) {
   $firstColumn[] = $pair[0];
   $secondColumn[] = $pair[1];
 };
+
+sort($firstColumn, SORT_NUMERIC);
+sort($secondColumn, SORT_NUMERIC);
+
+// $distance = 0;
+
+// for ($i = 0; $i < count($firstColumn); $i += 1) {
+//   $firstNum = $firstColumn[$i];
+//   $secondNum = $secondColumn[$i];
+
+//   if ($firstNum >= $secondNum) {
+//     $distance += $firstNum - $secondNum;
+//   } else {
+//     $distance += $secondNum - $firstNum;
+//   };
+// }
+
+$totalScore = 0;
+
+foreach($firstColumn as $firstNum) {
+  $count = 0;
+  forEach($secondColumn as $secondNum) {
+    if ($firstNum === $secondNum) {
+      $count += 1;
+    }
+  }
+  $totalScore += $firstNum * $count;
+}
+echo $totalScore;
