5b43ffe4d4f1c35fbfbd7d7c56acc0d3ff6dde2a
Radu Constantin <raduadrian.constantin94@gmail.com>
2024-12-05 20:13:56 +0200

    Done day 3

--- day_3.php
+++ day_3.php
@@ -1,6 +1,28 @@
 <?php
 $input = file_get_contents('./day_3_input.txt');
 
-preg_match_all('/mul\([0-9]{1,3},[0-9]{1,3}\)/', $input, $matches);
+preg_match_all("/mul\([0-9]{1,3},[0-9]{1,3}\)|do\(\)|don't\(\)/", $input, $matches);
 
-var_dump($matches[0][0]);
+$commands = $matches[0];
+
+$sum = 0;
+
+$enableAdding = true;
+
+foreach($commands as $command) {
+  if ($command === "do()") {
+    $enableAdding = true;  
+  } else if ($command === "don't()") {
+    $enableAdding = false;
+  } else {
+    if ($enableAdding) {
+      preg_match_all('/[0-9]+/',$command, $numbers);
+      $firstNumber = $numbers[0][0];
+      $secondNumber = $numbers[0][1];
+
+      $sum += $firstNumber * $secondNumber;
+    }
+  }
+}
+
+echo $sum;
